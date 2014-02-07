69b86bc5d8d321437665bb29237417bd3a215bd4
moupiya1 <mum316@psu.edu>
2014-02-07 17:00:03 -0500

    third commit hw

--- HW3.jl
+++ HW3.jl
@@ -10,6 +10,7 @@ A=randn(N)
 2.331e-6
 
 #2b. 
+#Write an array to files in asciii format
 function write_to_file()
     fin=open("writefile.txt","w+");
     A=randn(1000);
@@ -20,6 +21,7 @@ end
 
 
 #2b. 
+# read in ascii format
 function read_from_file()
 fr=open("writefile.txt","r");
 C=readcsv(fr,Float64);
@@ -36,6 +38,7 @@ end
 #reduces slightly.
 
 #2d.
+#Write an array to files in binary format
 function write_binary()
     fin=open("writebinary.bin","w+");
     B=randn(1000);
@@ -44,6 +47,7 @@ function write_binary()
     return B; #Just to print the values of A to check later with the read in values.
 end
 
+#Write an array to files in binary format
 function read_binary()
     fr=open("writebinary.bin");
 D=read(fr,Float64,1000);
@@ -52,7 +56,7 @@ D=read(fr,Float64,1000);
 end
 
 #For a large matrix
-function write_binary()
+function write_binary()      # writing a binary
     fin=open("writebinary.bin","w+");
     B=randn(1000,1000,100);
     write(fin,B);
@@ -61,7 +65,7 @@ function write_binary()
 end
 @elapsed x=write_binary()
 
-function read_binary()
+function read_binary() # reading a binary
     fr=open("writebinary.bin");
 D=read(fr,Float64,1000,1000,100);
     close(fr);
@@ -87,7 +91,7 @@ Aw=write_to_file()
 Ar=read_from_file()
 using Base.Test
 for i in 1:1000
-    @test Aw[i]==Ar[i]
+    @test Aw[i]==Ar[i]   # testing the equivalence of the array elements.
 end
 print("Test successful")
 
@@ -111,7 +115,7 @@ function read_from_file()
     end
 f=read_from_file()
 using Base.Test
-@test f!=0
+@test f!=0   # Does the file exist? If it does, it passes test.
 
 #Assert statements
 function read_from_file()
@@ -127,7 +131,7 @@ function read_from_file()
 
 #Testing whether the variance should use one pass or two pass algorithm.
 
-function v_one(y::Array)
+function v_one(y::Array) # This function calculates variance of a given array by one pass method.
 n = length(y);
 sum=zero(y[1]);
 ssqr= zero(y[1]);
@@ -136,10 +140,10 @@ ssqr=ssqr+y[i]*y[i];
 sum=sum+y[i];
 end;
 var=(1/(n-1))*(ssqr-sum*sum/n);
-return var;
+return var; #output of this function.
 end
     
-    function v_two(y::Array)
+    function v_two(y::Array) #this function gives the variance of an array calculated by two pass method.
 n = length(y);
 sum=zero(y[1]);
 for j in 1:n
@@ -151,22 +155,22 @@ for i in 1:n
 v2=v2+(y[i]-m1)^2;
 end;
 var2=(1/(n-1))*v2;
-return var2; 
+return var2; #output
 end
 
-function cal_variance(N::Int)
+function cal_variance(N::Int) # main function deciding and calling which funtion to use for variance calculating. N is the size of the array which would be an input.
    srand(42);
 true_mean = 10000.;
-y = true_mean+randn(N);
-    if N>10^6             #If array size is bigger than 10^8
+y = true_mean+randn(N);  # Creates an array of dimension N.
+    if N>10^6             #If array size is bigger than 10^8 then it takes too much time. The program quits.
         println("Array size too big to compute it in a reasonable amount of time")
-        elseif N>10^4              #If array size is bigger than 10^6
+        elseif N>10^4              #If array size is bigger than 10^6, one pass takes smaller time and it's better to use this.
             variance=v_one(y)
         println("Used one pass")
-        else 
+        else                                # If array size is smaller than 10^6, two pass is better as it gives more accuracy.
             variance=v_two(y)
         println("Used two pass")
         end
- return variance;
+ return variance; # output. Gives an error whenarray size is greater tha 10^8
 end
  
